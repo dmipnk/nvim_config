2b41c068c0e068ba4da92f344945c0dfa5b556ea
melifaro <dmitrijpankratov727@gmail.com>
2025-11-23 21:06:41 +0300

    my changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -130,7 +130,6 @@ return {
     -- - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
     -- - settings (table): Override the default settings passed when initializing the server.
     local servers = {
-      ts_ls = {},
       ruff = {},
       pylsp = {
         settings = {
@@ -149,13 +148,11 @@ return {
         },
       },
       html = { filetypes = { 'html', 'twig', 'hbs' } },
-      cssls = {},
       tailwindcss = {},
       dockerls = {},
-      sqlls = {},
-      terraformls = {},
       jsonls = {},
       yamlls = {},
+      rust_analyzer = {},
       lua_ls = {
         settings = {
           Lua = {
